e351b907f9c75afff0b05bb2d379980d87bda719
Ibi <ibi.coleman@gmail.com>
2022-03-08 19:33:31 -0500

    pairpro

--- exercise/src/main/java/com/techelevator/MakeChange.java
+++ exercise/src/main/java/com/techelevator/MakeChange.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
  Write a command line program which prompts the user for the total bill, and the amount tendered. It should then
  display the change required.
@@ -12,6 +14,24 @@ package com.techelevator;
 public class MakeChange {
 
 	public static void main(String[] args) {
+
+		Scanner keyboard = new Scanner(System.in);
+		double billAmount = 0;
+		double tendered = 0;
+		double change = 0;
+
+		String input = "";
+
+      System.out.println("Please Enter Bill Total");
+	  input = keyboard.nextLine();
+	  billAmount = Double.parseDouble(input);
+
+	  System.out.println("Please Enter Total Amount Tendered");
+	  input = keyboard.nextLine();
+	  tendered = Double.parseDouble(input);
+
+	  change = (tendered - billAmount);
+	  System.out.printf("Total Required Change Back %.2f\n", change);
 
 	}
 
--- exercise/src/main/java/com/techelevator/MartianWeight.java
+++ exercise/src/main/java/com/techelevator/MartianWeight.java
@@ -1,5 +1,6 @@
 package com.techelevator;
 
+import java.util.Scanner;
 /*
 In case you've ever pondered how much you weigh on Mars, here's the calculation:
  	Wm = We * 0.378
@@ -19,6 +20,17 @@ Enter a series of Earth weights (space-separated): 98 235 185
 public class MartianWeight {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a series of Earth weights(space seperated): ");
+		String userInput = input.nextLine();
+		String[] earthWeight = userInput.split(" ");
+		int[] intWeight = new int[earthWeight.length];
+
+		for (int i = 0; i < earthWeight.length; i++) {
+			intWeight[i] = Integer.parseInt(earthWeight[i]);
+			double weight = intWeight[i] * 0.378;
+			System.out.println(intWeight[i] + " lbs. on Earth is " + weight + "lbs. on Mars." );
+		}
 
 	}
 
